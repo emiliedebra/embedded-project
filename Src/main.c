2db64927040ecbb063a2e6b96bcb19cc97de73f5
Emilie <Emilie@196.42.77.80>
2017-10-02 05:37:49 +0200

    Clean Up Code

--- Src/main.c
+++ Src/main.c
@@ -1,79 +1,41 @@
 /**
   ******************************************************************************
   * @file    Audio/Audio_playback_and_record/Src/main.c 
-  * @author  MCD Application Team
+  * @author  ES Team 5
   * @version V1.3.5
-  * @date    17-February-2017
+  * @date    October-2017
   * @brief   Main program body.
   ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2017 STMicroelectronics International N.V. 
-  * All rights reserved.</center></h2>
-  *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
-  *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
-  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */ 
+**/
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+
 #define TXBUFFERSIZE 1
 #define RXBUFFERSIZE 1
+
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef UartHandle;
 __IO ITStatus UartReady = RESET;
 
 TIM_HandleTypeDef TimHandle;
-uint32_t uwPrescalerValue = 0;
 TIM_HandleTypeDef hTimLed;
+uint32_t uwPrescalerValue = 0;
 
-//TIM_OC_InitTypeDef sConfigLed;
 uint8_t aTxBuffer[TXBUFFERSIZE];
 uint8_t aRxBuffer[RXBUFFERSIZE];
 
 /* Counter for User button presses. Defined as external in waveplayer.c file */
 __IO uint32_t PressCount = 0;
-
-uint8_t beat[1] = {0};
-uint8_t buttons[2] = {0x00, 0x00};
-uint8_t beatFlag = 0;
+__IO uint32_t PbPressCheck = 0;
 
 /* Wave Player Pause/Resume Status. Defined as external in waveplayer.c file */
-__IO uint32_t PauseResumeStatus = IDLE_STATUS;   
-                                                   
+__IO uint32_t PauseResumeStatus = IDLE_STATUS;
+__IO uint32_t CmdIndex = CMD_PLAY;
+
 extern uint32_t AudioPlayStart;
 
 /* Re-play Wave file status on/off.
@@ -86,25 +48,25 @@ __IO uint16_t CCR1Val = 16826;
                                             
 extern __IO uint32_t LEDsState;
 
-/* Save MEMS ID */
+/* Beat Tracking Variables */
+uint8_t beat[1] = {0};
+uint8_t buttons[2] = {0x00, 0x00};
+uint8_t beatFlag = 0;
 
-__IO uint32_t CmdIndex = CMD_PLAY;
-__IO uint32_t PbPressCheck = 0;
-
+/* USB Variables */
 FATFS USBDISKFatFs;          /* File system object for USB disk logical drive */
 char USBDISKPath[4];         /* USB Host logical drive path */
 USBH_HandleTypeDef hUSBHost; /* USB Host handle */
-
-MSC_ApplicationTypeDef AppliState = APPLICATION_IDLE;
 static uint8_t  USBH_USR_ApplicationState = USBH_USR_FS_INIT;
 
+MSC_ApplicationTypeDef AppliState = APPLICATION_IDLE;
+
 /* Private function prototypes -----------------------------------------------*/
-// static void TIM_LED_Config(void);
 static void SystemClock_Config(void);
 static void USBH_UserProcess(USBH_HandleTypeDef *pHost, uint8_t vId);
 static void MSC_Application(void);
 void playBeat();
-// void InitializeTimer();
+
 /* Private functions ---------------------------------------------------------*/
 
 /**
@@ -127,6 +89,7 @@ int main(void)
   BSP_LED_Init(LED4);
   BSP_LED_Init(LED5);
   BSP_LED_Init(LED6);
+
   /* Configure the system clock to 168 MHz */
   SystemClock_Config();
 
@@ -142,24 +105,21 @@ int main(void)
 	  /* Initialization Error */
 	  Error_Handler();
   }
-  /*##-2- Start the TIM Base generation in interrupt mode ####################*/
+  /* Start the TIM Base generation in interrupt mode*/
   /* Start Channel1 */
   if(HAL_TIM_Base_Start_IT(&TimHandle) != HAL_OK)
   {
 	  /* Starting Error */
 	  Error_Handler();
   	  }
-  /* Turn ON LED4: start of application */
-  BSP_LED_On(LED4);
-  
+
   /* Initialize the Repeat state */
   RepeatState = REPEAT_ON;
 
-  
   /* Configure USER Button */
   BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_EXTI);
   
-  /* Config UART */
+  /* UART Config */
   UartHandle.Instance = USARTx;
   UartHandle.Init.BaudRate = 9600;
   UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
@@ -170,8 +130,12 @@ int main(void)
   UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
 
   if(HAL_UART_Init(&UartHandle) != HAL_OK) {
+	  /* Starting Error */
 	  Error_Handler();
   }
+
+  /* Turn ON LED4: start of application */
+  BSP_LED_On(LED4);
 
   /*##-1- Link the USB Host disk I/O driver ##################################*/
   if(FATFS_LinkDriver(&USBH_Driver, USBDISKPath) == 0)
@@ -210,7 +174,6 @@ int main(void)
 			}
 
 			//move to the next beat
-			//beat[0] == 1 ? beat[0]=0 : beat[0]++;
 			if (beat[0] == 1){
 				beat[0] = 0;
 			}
@@ -241,71 +204,23 @@ void playBeat() {
 	CmdIndex = CMD_PLAY;
 	BSP_LED_On(LED5);
 	WavePlayerStart();
-	//WavePlayerStop();
-	// HAL_Delay(10);
 	BSP_LED_Off(LED5);
-	/*
-	// send command 0x01 (turn LEDs on)
-	  aTxBuffer[0] = 0x01;
-	  if(HAL_UART_Transmit_IT(&UartHandle, (uint8_t*)aTxBuffer, TXBUFFERSIZE)!= HAL_OK){
-		Error_Handler();
-	  }
-	  BSP_LED_On(LED4);
-
-	  //delay
-	  HAL_Delay(1000);
-
-	  //send command 0x02 (turn LEDs off)
-	  aTxBuffer[0] = 0x02;
-	  if(HAL_UART_Transmit_IT(&UartHandle, (uint8_t*)aTxBuffer, TXBUFFERSIZE)!= HAL_OK){
-		Error_Handler();
-	  }
-	  BSP_LED_Off(LED4);
-
-	  //delay
-	  HAL_Delay(1000);
-
-	  */
-//	BSP_LED_On(LED6);
-//		//send beat to F0
-//	if(HAL_UART_Transmit_IT(&UartHandle, (uint8_t *)beat, 1)!= HAL_OK){
-//		Error_Handler();
-//	}
-//
-//	//receive button array
-//	if(HAL_UART_Receive_IT(&UartHandle, (uint8_t *)buttons, 2)!= HAL_OK){
-//		Error_Handler();
-//	}
-//	while(UartReady != SET){}
-//	UartReady = RESET;
-//
-//	//play the beat sound
-//	CmdIndex = CMD_PLAY;
-
-	// BSP_LED_Off(LED6);
-//
-//
-//	//increment the beat
-//	beat[0] == 1 ? beat[0]=0 : beat[0]++;
 }
 
 
-//BEAT TIMER INTERRUPT
+/* Beat Timer Interrupt */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *TimHandle)
 {
 	BSP_LED_Toggle(LED6);
-
 	beatFlag = 1;
-
-
 }
 
-//TRANSFER INTERRUPT
+/* Transfer Interrupt */
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle) {
 	UartReady = SET;
 }
 
-//RECEIVE INTERRUPT
+/* Receiver Interrupt */
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *UartHandle) {
 	UartReady = SET;
 }
@@ -465,7 +380,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 	  CmdIndex = CMD_PLAY;
 	  BSP_LED_On(LED4);
 	  WavePlayerStart();
-	  //HAL_Delay(10);
+	  HAL_Delay(10);
 	  BSP_LED_Off(LED4);
   }
 }
